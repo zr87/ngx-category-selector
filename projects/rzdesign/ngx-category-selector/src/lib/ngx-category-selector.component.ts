b450c02c9e4e30ac3c5b893b8d32dd32d2889739
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2020-10-20 20:02:36 +0200

    Implement ControlValueAccessor

--- projects/rzdesign/ngx-category-selector/src/lib/ngx-category-selector.component.ts
+++ projects/rzdesign/ngx-category-selector/src/lib/ngx-category-selector.component.ts
@@ -3,19 +3,32 @@ import {
   EventEmitter,
   Input,
   Output,
-  OnInit
+  OnInit, forwardRef
 } from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const CATEGORY_CONTROL_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => NgxCategorySelectorComponent),
+  multi: true
+};
 
 @Component({
   selector: 'lib-ngx-category-selector',
+  providers: [CATEGORY_CONTROL_ACCESSOR],
   templateUrl: './ngx-category-selector.component.html',
   styleUrls: ['./ngx-category-selector.component.scss']
 })
-export class NgxCategorySelectorComponent implements OnInit {
+export class NgxCategorySelectorComponent implements OnInit, ControlValueAccessor {
 
   preselection: (string | number)[] | null = [];
   private preselectionSubject = new BehaviorSubject<CategoryNode>(null);
+
+  // tslint:disable-next-line:ban-types
+  private onTouch: Function;
+  // tslint:disable-next-line:ban-types
+  private onModelChange: Function;
 
   @Input() data: CategoryNode[];
 
@@ -29,6 +42,18 @@ export class NgxCategorySelectorComponent implements OnInit {
   constructor() {
   }
 
+  writeValue(value: CategoryNode): void {
+    this.preselectionSubject.next(value || null);
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onModelChange = fn;
+  }
+
   ngOnInit(): void {
     this.preselectionSubject
       .subscribe(value => {
@@ -39,6 +64,11 @@ export class NgxCategorySelectorComponent implements OnInit {
           this.preselection = this.getSelectionHierarchy(this.data, value);
         }
       });
+  }
+
+  onCategorySelection(event): void {
+    this.selection.emit(event);
+    this.onModelChange(event);
   }
 
 
--- projects/rzdesign/ngx-category-selector/src/lib/ngx-category-selector.module.ts
+++ projects/rzdesign/ngx-category-selector/src/lib/ngx-category-selector.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { NgxCategorySelectorComponent } from './ngx-category-selector.component';
 import { CategorySetComponent } from './category-set/category-set.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [NgxCategorySelectorComponent, CategorySetComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [NgxCategorySelectorComponent]
 })
 export class NgxCategorySelectorModule { }
